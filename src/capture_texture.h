c06efa987264dee735f9855ce2236a405a7b375d
Edward Flick <directrix1@gmail.com>
2024-12-22 05:42:51 -0600

    Part 1: Updating a texture from C++ GDExtension.

--- src/capture_texture.h
+++ src/capture_texture.h
@@ -1,37 +1,34 @@
 #pragma once
 
-#include <godot_cpp/classes/global_constants.hpp>
-#include <godot_cpp/classes/texture2d.hpp>
 #include <godot_cpp/classes/image.hpp>
+#include <godot_cpp/classes/image_texture.hpp>
 #include <godot_cpp/core/class_db.hpp>
 
 namespace godot {
 
-class CaptureTexture : public Texture2D {
-    GDCLASS(CaptureTexture, Texture2D);
+class CaptureTexture : public ImageTexture {
+	GDCLASS(CaptureTexture, ImageTexture);
 
 private:
-    // We'll store our image in here.
-    Ref<Image> internal_image;
+	// We'll store our image in here.
+	Ref<Image> internal_image;
 
 protected:
-    static void _bind_methods();
+	static void _bind_methods();
 
 public:
-    CaptureTexture();
-    ~CaptureTexture() override = default;
+	CaptureTexture();
+	~CaptureTexture() override = default;
 
-    // Called after the object is initialized in Godot.
-    void _ready();
+	// Called after the object is initialized in Godot.
+	void _ready();
+	void _process(double delta);
 
-    // Called every frame (if we want automated updates).
-    void _process(double delta);
+	// Helper to initialize your image with a desired size and format.
+	void create_internal_image(int width, int height);
 
-    // Example function to manually update texture from code.
-    void update_texture_contents();
-
-    // Helper to initialize your image with a desired size and format.
-    void create_internal_image(int width, int height, bool use_filter = false);
+	// Manually update pixels and reupload
+	void update_texture_contents();
 };
 
 } // namespace godot
