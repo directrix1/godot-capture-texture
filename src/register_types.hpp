23a202cd84f14a926a5bbea752aa2aac0c6b3da9
Edward Flick <directrix1@gmail.com>
2024-12-22 13:56:49 -0600

    Added compile flags, and use c++ file naming conventions.

---
+++ src/register_types.hpp
@@ -0,0 +1,7 @@
+#ifndef CAPTURE_TEXTURE_REGISTER_TYPES_HPP
+#define CAPTURE_TEXTURE_REGISTER_TYPES_HPP
+
+void initialize_gdextension_types();
+void uninitialize_gdextension_types();
+
+#endif // CAPTURE_TEXTURE_REGISTER_TYPES_HPP
